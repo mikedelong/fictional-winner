9718eef0471eda206a16c1ee1a5e88b7d0fd2647
mikedelong <mikedelong@outlook.com>
2020-04-20 08:59:03 -0400

    feature: read data from the 538 github repository

--- main_538.py
+++ main_538.py
@@ -2,11 +2,23 @@ from logging import INFO
 from logging import basicConfig
 from logging import getLogger
 from time import time
+from io import StringIO
 
+import requests
+import pandas as pd
 
 if __name__ == '__main__':
     time_start = time()
     logger = getLogger(__name__)
     basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=INFO)
     logger.info('started.')
+
+    url = 'https://projects.fivethirtyeight.com/polls-page/president_polls.csv'
+
+    df = pd.read_csv(url)
+
+    logger.info(df.shape)
+    logger.info('\n{}'.format(df.head(5)))
+
+
     logger.info('total time: {:5.2f}s'.format(time() - time_start))
