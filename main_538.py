0f1833f7906cc7b3345701a8049a6a00b2ffd327
mikedelong <mikedelong@outlook.com>
2020-04-22 09:28:00 -0400

    refactor: rename the end date data

--- main_538.py
+++ main_538.py
@@ -26,19 +26,21 @@ if __name__ == '__main__':
     national_df = df[df.state.isnull()].copy(deep=True)
     logger.info(national_df.shape)
     bt_df = national_df[national_df.answer.isin(['Biden', 'Trump'])].drop_duplicates()
-    biden_df = bt_df[bt_df.answer.isin(['Biden'])][['end_date', 'pct']].rename(columns={'pct': 'Biden'})
-    trump_df = bt_df[bt_df.answer.isin(['Trump'])][['end_date', 'pct']].rename(columns={'pct': 'Trump'})
+    biden_df = bt_df[bt_df.answer.isin(['Biden'])][['end_date', 'pct']].rename(
+        columns={'pct': 'Biden', 'end_date': 'date'}, )
+    trump_df = bt_df[bt_df.answer.isin(['Trump'])][['end_date', 'pct']].rename(
+        columns={'pct': 'Trump', 'end_date': 'date'}, )
     fig0, ax0 = plt.subplots()
-    biden_df.set_index('end_date').plot(ax=ax0, c='blue', label='Biden', style='.', )
+    biden_df.set_index('date').plot(ax=ax0, c='blue', label='Biden', style='.', )
     # https://stackoverflow.com/questions/17638137/curve-fitting-to-a-time-series-in-the-format-datetime
-    biden_date_numbers = mdates.date2num(biden_df.end_date.values)
+    biden_date_numbers = mdates.date2num(biden_df.date.values)
     for degree in range(1, 4):
         biden_fit = np.polyfit(x=biden_date_numbers, y=biden_df.Biden, deg=degree)
         biden_poly = np.poly1d(biden_fit)
         ax0.plot(mdates.num2date(biden_date_numbers), biden_poly(biden_date_numbers), 'b-')
 
-    trump_df.set_index('end_date').plot(ax=ax0, c='red', label='Trump', style='.', )
-    trump_date_numbers = mdates.date2num(trump_df.end_date.values)
+    trump_df.set_index('date').plot(ax=ax0, c='red', label='Trump', style='.', )
+    trump_date_numbers = mdates.date2num(trump_df.date.values)
     for degree in range(1, 4):
         trump_fit = np.polyfit(x=trump_date_numbers, y=trump_df.Trump, deg=1)
         trump_poly = np.poly1d(trump_fit)
@@ -48,8 +50,8 @@ if __name__ == '__main__':
     plt.savefig(scatter_png)
     fig1, ax1 = plt.subplots()
 
-    biden_df['days'] = biden_df['end_date'].apply(lambda x: (x.to_pydatetime() - biden_df['end_date'].min()).days)
-    trump_df['days'] = trump_df['end_date'].apply(lambda x: (x.to_pydatetime() - trump_df['end_date'].min()).days)
+    biden_df['days'] = biden_df['date'].apply(lambda x: (x.to_pydatetime() - biden_df['date'].min()).days)
+    trump_df['days'] = trump_df['date'].apply(lambda x: (x.to_pydatetime() - trump_df['date'].min()).days)
     sns.regplot(ax=ax1, data=biden_df, x='days', y='Biden', )
     sns.regplot(ax=ax1, data=trump_df, x='days', y='Trump', )
     regplot_png = './biden_trump_regplot.png'
