6f7285393e771f1ae0533b20f65cba7b437fc2d0
mikedelong <mikedelong@outlook.com>
2020-04-23 09:11:52 -0400

    feature: check the two datasets for consistency at the state level

--- states.py
+++ states.py
@@ -22,5 +22,13 @@ if __name__ == '__main__':
     df = df[~df.state.isnull()]
     logger.info(sorted(df.state.unique()))
     logger.info(df.state.nunique())
+    polls_no_electoral_college = [state for state in sorted(df.state.unique()) if state not in electoral_college.keys()]
+    electoral_college_no_polls = [state for state in sorted(electoral_college.keys()) if state not in df.state.values]
+    if len(polls_no_electoral_college):
+        for item in polls_no_electoral_college:
+            logger.warning('no Electoral College data for {}'.format(item))
+    if len(electoral_college_no_polls):
+        for item in electoral_college_no_polls:
+            logger.warning('no polls for {}'.format(item))
 
     logger.info('total time: {:5.2f}s'.format(time() - time_start))
