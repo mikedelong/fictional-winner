d29e711e2713aa10a79ee1d0b7f956d1f7ae0c4b
mikedelong <mikedelong@outlook.com>
2020-04-30 08:24:55 -0400

    todo: added a couple of TODOs

--- states.py
+++ states.py
@@ -168,11 +168,16 @@ if __name__ == '__main__':
         plt.scatter(x=graph_df.date, y=graph_df.Trump, c='r', )
         plt.savefig('./states-daily-matplotlib.png')
     else:
+        # todo fix dates on x axis
+        # https://stackoverflow.com/questions/29308729/can-i-plot-a-linear-regression-with-datetimes-on-the-x-axis-with-seaborn
+        # todo fix axis labels
         graph_df['numbers'] = mdates.date2num(graph_df.date.values)
         sns.regplot(ax=ax, color='b', data=graph_df, x='numbers', y='Biden', )
         sns.regplot(ax=ax, color='b', data=graph_df, x='numbers', y='Biden', lowess=True, scatter=False, )
+        sns.regplot(ax=ax, color='b', data=graph_df, x='numbers', y='Biden', logx=True, scatter=False, )
         sns.regplot(ax=ax, color='r', data=graph_df, x='numbers', y='Trump', )
         sns.regplot(ax=ax, color='r', data=graph_df, x='numbers', y='Trump', lowess=True, scatter=False, )
+        sns.regplot(ax=ax, color='r', data=graph_df, x='numbers', y='Trump', logx=True, scatter=False, )
         plt.savefig('./states-daily-regplot.png')
 
     logger.info('total time: {:5.2f}s'.format(time() - time_start))
