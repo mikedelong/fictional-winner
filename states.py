4c6ed17b62948f602a3d5dc49be8ba569605b680
mikedelong <mikedelong@outlook.com>
2020-05-10 14:36:24 -0400

    cleanup: remove code we have factored out into a separate file

--- states.py
+++ states.py
@@ -55,38 +55,6 @@ def get_results(arg_df, arg_cutoff_date, electoral_df, historical_df, verbose, )
     return result_democrat_votes, result_republican_votes, result_ranked
 
 
-def get_realization(arg_df, arg_cutoff_date, electoral_df, historical_df, ):
-    polling = {}
-    arg_df = arg_df[arg_df.end_date <= arg_cutoff_date]
-    for state in arg_df.state.unique():
-        polling[state] = {}
-        this_df = arg_df[arg_df.state == state]
-        this_df = this_df[this_df.end_date == this_df.end_date.max()]
-        for candidate in [democrat, republican]:
-            polling[state][candidate] = this_df[this_df.answer.isin({candidate})].groupby('pct').mean().index[0]
-    result_democrat_votes = 0
-    result_republican_votes = 0
-    review_unique = review_2016_df.State.unique()
-    for state in electoral_df.state.unique():
-        if state in polling.keys():
-            poll = polling[state]
-            votes = electoral_df[electoral_df.state == state].votes.values[0]
-            democrat_pct = poll[democrat]
-            republican_pct = poll[republican]
-            if abs(democrat_pct - republican_pct) < 15.1:
-                simulated_democrat_result = binomial(n=1, p=democrat_pct / (democrat_pct + republican_pct))
-            else:
-                simulated_democrat_result = int((1 + copysign(1, democrat_pct - republican_pct)) / 2)
-            result_democrat_votes += votes * simulated_democrat_result
-            result_republican_votes += votes * (1 - simulated_democrat_result)
-        elif state in review_unique:
-            result_democrat_votes += historical_df[historical_df.State == state].electoralDem.values[0]
-            result_republican_votes += historical_df[historical_df.State == state].electoralRep.values[0]
-        else:
-            logger.warning('missing state: {}'.format(state), )
-    return result_democrat_votes, result_republican_votes
-
-
 if __name__ == '__main__':
     time_start = time()
     logger = getLogger(__name__)
@@ -126,39 +94,6 @@ if __name__ == '__main__':
     else:
         logger.info('total: {}: {} {}: {}'.format(democrat, democrat_votes, republican, republican_votes, ))
 
-    realizations = list()
-    realization_count = 100
-    count_democrat = 0
-    count_republican = 0
-    democrat_realizations = list()
-    for index, realization in enumerate(range(realization_count)):
-        realization_democrat, realization_republican = get_realization(arg_df=a2_df.copy(deep=True),
-                                                                       arg_cutoff_date=cutoff_date,
-                                                                       electoral_df=electoral_college_df,
-                                                                       historical_df=review_2016_df, )
-        count_democrat += 1 if realization_democrat > realization_republican else 0
-        count_republican += 1 if realization_democrat < realization_republican else 0
-        format_string = '{} {}: {} {}: {} {}: {} {}: {} ratio: {:5.4f} mean: {:5.1f} median: {}'
-        democrat_realizations = [item[0] for item in realizations]
-        if len(democrat_realizations):
-            logger.info(format_string.format(index, democrat, realization_democrat, republican, realization_republican,
-                                             democrat, count_democrat, republican, count_republican,
-                                             count_democrat / (count_democrat + count_republican),
-                                             np.array(democrat_realizations).mean(),
-                                             int(np.median(np.array(democrat_realizations)), ), ))
-        realizations.append((realization_democrat, realization_republican,))
-    bin_count = max(democrat_realizations) - min(democrat_realizations) + 1
-    democrat_win_realizations = [item for item in democrat_realizations if item >= 270]
-    democrat_lose_realizations = [item for item in democrat_realizations if item < 270]
-    logger.info('{} simulated wins: {} out of {} realizations'.format(democrat, len(democrat_win_realizations),
-                                                                      len(democrat_realizations),
-                                                                      len(democrat_win_realizations) / len(
-                                                                          democrat_realizations), ))
-    logger.info('{} mean outcome: {:5.2f} median outcome: {}'.format(democrat, np.array(democrat_realizations).mean(),
-                                                                     np.median(np.array(democrat_realizations))), )
-    plt.hist(x=democrat_win_realizations, bins=bin_count, color='blue', )
-    plt.hist(x=democrat_lose_realizations, bins=bin_count, color='red', )
-    plt.savefig('./{}-histogram.png'.format(democrat.lower(), ), )
     graph_df = pd.DataFrame(columns=['date', democrat, republican, ], )
     lm_df = pd.DataFrame(columns=['date', 'votes', 'candidate', ], )
     for cutoff_date in sorted(a2_df.end_date.unique(), ):
