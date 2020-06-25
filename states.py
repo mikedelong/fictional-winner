784d85604e5efc3c56e58e3ecf5754ccb48269f8
mikedelong <mikedelong@outlook.com>
2020-06-25 09:03:38 -0400

    todo: added a TODO

--- states.py
+++ states.py
@@ -72,6 +72,7 @@ def get_results(arg_df, arg_cutoff_date, electoral_df, historical_df, verbose, )
     return result_democrat, result_republican, result_ranked
 
 
+# todo use clf() instead of figure/del figure
 if __name__ == '__main__':
     time_start = time()
     logger = getLogger(__name__)
