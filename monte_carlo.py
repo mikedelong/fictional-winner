7a6fe49ed08c78afda77af356755f300802f7022
mikedelong <mikedelong@outlook.com>
2020-06-28 10:41:55 -0400

    cleanup: remove a completed TODO

--- monte_carlo.py
+++ monte_carlo.py
@@ -54,7 +54,6 @@ def get_realization(arg_df, arg_cutoff_date, electoral_df, historical_df, arg_de
     return result_democrat_votes, result_republican_votes
 
 
-# todo add historical calculation of the Monte Carlo median
 # todo make the output folder if it does not exist
 if __name__ == '__main__':
     time_start = time()
