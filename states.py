c902250b12cfc803bce072fcc6f4e6e1aa3bcb4d
mikedelong <mikedelong@outlook.com>
2020-07-06 08:21:03 -0400

    feature: tighten the layout and add the figure size using aspect/height

--- states.py
+++ states.py
@@ -167,8 +167,8 @@ if __name__ == '__main__':
             clf()
         elif plot_style == plot_styles[1]:
             lm_df['date'] = date2num(lm_df.date.values, )
-            plot = lmplot(data=lm_df, hue='candidate', order=3, palette=palette, x='date', y='votes', ).set(
-                xlim=(lm_df.date.min() - 100, lm_df.date.max() + 100,), ylim=(100, 450,), )
+            plot = lmplot(aspect=1.6, data=lm_df, height=10, hue='candidate', order=3, palette=palette, x='date',
+                          y='votes', ).set(xlim=(lm_df.date.min() - 100, lm_df.date.max() + 100,), ylim=(100, 450,), )
             plot.set_xticklabels(labels=[num2date(number, tz=None, ).date() for number in lm_df.date.values], )
             lmplot_png = '{}daily-lmplot.png'.format(output_folder)
             for axes in plot.axes.flat:
