38afe6733646caf48a8cd99b26154ab8a58a590f
mikedelong <mikedelong@outlook.com>
2020-07-06 08:41:08 -0400

    todo: added a TODO

--- monte_carlo.py
+++ monte_carlo.py
@@ -183,6 +183,7 @@ if __name__ == '__main__':
         style.use('fivethirtyeight')
         hist(x=democrat_win_realizations, bins=bin_count, color='blue', )
         hist(x=democrat_lose_realizations, bins=bin_count, color='red', )
+        # todo set the minimum and maximum values on the X-axis
         savefig('{}{}-{}-histogram.png'.format(output_folder, democrat.lower(), cutoff_date.date(), ), )
         clf()
         median_map[cutoff_date] = realization_median
