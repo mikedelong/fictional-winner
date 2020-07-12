8b3f5b6c3cbc50be17363e5b01652f3162a519b1
mikedelong <mikedelong@outlook.com>
2020-07-12 10:24:26 -0400

    cleanup: remove a completed TODO

--- states.py
+++ states.py
@@ -307,7 +307,6 @@ if __name__ == '__main__':
             savefig(rank_scatterplot_png, )
             clf()
             ax_bar = barplot(data=rank_df, hue='candidate', x='State', y='abs_margin', )
-            # todo use output folder
             rank_barplot_png = '{}rank-barplot.png'.format(output_folder)
             logger.info('saving {} to {}'.format(plot_style, rank_barplot_png, ), )
             savefig(rank_barplot_png, )
