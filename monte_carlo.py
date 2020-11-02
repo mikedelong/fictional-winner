b0c3b110a300a7833cb7be4ff7b0f6bace6d2359
mikedelong <mikedelong@outlook.com>
2020-11-02 07:59:19 -0500

    cleanup: remove a completed TODO

--- monte_carlo.py
+++ monte_carlo.py
@@ -58,7 +58,6 @@ def get_realization(arg_df, arg_cutoff_date, electoral_df, historical_df, arg_de
     return int(result_democrat_votes), int(result_republican_votes)
 
 
-# todo refactor this so it produces a data file of realizations and lets us calculate forward from the most recent
 if __name__ == '__main__':
     time_start = time()
     logger = getLogger(__name__)
